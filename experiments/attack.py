c8a9c45675a161397c64aba1afd0a0f438e44152
karl-richter <karl.richter@tum.de>
2022-05-29 23:03:51 +0200

    feat: added shap calculation

--- experiments/attack.py
+++ experiments/attack.py
@@ -1,4 +1,7 @@
 import logging
+
+# import numpy as np
+import shap
 import torch
 from style_paraphrase.inference_utils import GPT2Generator
 from tqdm import tqdm
@@ -19,15 +22,23 @@ def load_model(model_name: str):
 def read_data(file_path):
     import pandas as pd
 
-    data = pd.read_csv(file_path, sep="\t").values.tolist()
+    data = pd.read_table(file_path).values.tolist()
     return data
 
 
 def write_data(attack_data, output_file_path):
-    with open(output_file_path, "w") as f:
-        print("p_val", "\t", "orig_sent", "\t", "adv_sent", "\t", "labels", file=f)
-        for p_val, orig_sent, adv_sent, label in attack_data:
-            print(p_val, "\t", orig_sent, "\t", adv_sent, "\t", label, file=f)
+    import pandas as pd
+
+    pd.DataFrame(
+        attack_data,
+        columns=[
+            "result_type",
+            "original_text",
+            "perturbed_text",
+            "original_output",
+            "perturbed_output",
+        ],
+    ).to_csv(output_file_path, sep="\t", index=False)
 
 
 def get_predict_label(model, sent, tokenizer):
@@ -43,9 +54,58 @@ def get_predict_label(model, sent, tokenizer):
     return predict
 
 
+def predict_labels(data):
+    import numpy as np
+    import math
+
+    model = load_model(model_name="textattack/bert-base-uncased-SST-2")
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+
+    # Tokenize inputs
+    embeddings = tokenizer(list(data), return_tensors="pt", padding=True).input_ids
+    # Predict
+    prediction = (
+        model(embeddings.cuda() if torch.cuda.is_available() else embeddings.cpu())
+        .logits.detach()
+        .numpy()
+    )
+    scores = (np.exp(prediction).T / np.exp(prediction).sum(-1)).T
+    val = [1 / (1 + math.exp(-x)) for x in scores[:, 1]]
+    return val
+
+
+def transform_attack_data(attack_data):
+    import numpy as np
+    import pandas as pd
+
+    adv_examples = pd.DataFrame(
+        attack_data,
+        columns=[
+            "result_type",
+            "original_text",
+            "perturbed_text",
+            "original_output",
+            "perturbed_output",
+        ],
+    )
+
+    # Filter out skipped and unsuccessful attacks
+    adv_examples = adv_examples[adv_examples["result_type"] == "Failed"]
+
+    adv_x_test = np.array(adv_examples["perturbed_text"])
+    adv_y_test = np.array(adv_examples["original_output"])
+
+    # adv_x_test = tokenizer(
+    #    list(adv_x_test), return_tensors="pt", padding=True
+    # ).input_ids
+
+    # adv_y_test = list(adv_y_test)
+    return adv_x_test, adv_y_test
+
+
 def main(params: dict):
-    victim_model = load_model(model_name=params.model_name)
     orig_data = read_data(params.orig_file_path)
+    victim_model = load_model(model_name=params.model_name)
     tokenizer = AutoTokenizer.from_pretrained(params.bert_type)
     paraphraser = GPT2Generator(params.model_dir, upper_length="same_5")
 
@@ -55,12 +115,13 @@ def main(params: dict):
     paraphraser.modify_p(params.p_val)
 
     for sent, label in tqdm(orig_data):
-        if (
-            label != params.orig_label
-            or get_predict_label(victim_model, sent, tokenizer) != params.orig_label
-        ):
-            continue
-
+        # filter for params.orig_label to only data for the target label (aka directed attack)
+        # if (
+        #    label != params.orig_label
+        #    or get_predict_label(victim_model, sent, tokenizer) != params.orig_label
+        # ):
+        #    continue
+        # flag indicates success of the attack
         flag = False
         logger.info(sent)
         generated_sent = [sent for _ in range(params.iter_epochs)]
@@ -69,16 +130,29 @@ def main(params: dict):
             logger.info(paraphrase_sent)
             predict = get_predict_label(victim_model, paraphrase_sent, tokenizer)
             if predict != label:
-                attack_data.append((1, sent, paraphrase_sent, label))
+                attack_data.append(
+                    ("Successful", sent, paraphrase_sent, label, predict)
+                )
                 flag = True
                 mis += 1
+                # can be improved, in the paper the authors indicate they use sentenceBERT
+                # to select the best paraphrase, here they only select the first that succeeds
                 break
-        if flag:
-            pass
-        else:
-            attack_data.append((-1, sent, sent, label))
+        if not flag:
+            attack_data.append(("Failed", sent, sent, label, label))
         logger.info("------------------")
 
         total += 1
     logger.info(f"Completed attack, success rate: {mis}/{total}")
+
+    # checkpoint the attack data
     write_data(attack_data=attack_data, output_file_path=params.output_file_path)
+
+    # transform the attack data to be used for SHAP
+    adv_x_test, adv_y_test = transform_attack_data(attack_data)
+
+    # initialise the SHAP explainer
+    explainer = shap.Explainer(predict_labels, tokenizer)
+
+    # calculate the SHAP values
+    shap_values = explainer(adv_x_test, fixed_context=1)
